dab05ba8a495d66bba3da025fc76e518d55b09e2
chico <chico.zimcolor@gmail.com>
2022-07-04 19:51:03 -0300

    added puppeteer dependency

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,5 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "node",
+  preset: "./jestPresets",
   moduleDirectories: ["node_modules", "src"],
 };
-
