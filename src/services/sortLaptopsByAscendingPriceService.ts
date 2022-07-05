c95ba5ebcf777d872dbac63407bba1a2c38577dd
chico <chico.zimcolor@gmail.com>
2022-07-05 04:02:10 -0300

    implemented SortLaptopsByAscendingPriceService

--- src/services/sortLaptopsByAscendingPriceService.ts
+++ src/services/sortLaptopsByAscendingPriceService.ts
@@ -2,6 +2,6 @@ import Laptop from "entities/laptop";
 
 export default class SortLaptopsByAscendingPriceService {
   execute(laptops: Laptop[]): Laptop[] {
-    return laptops.sort((a: Laptop, b: Laptop) => a.pricesByHDD[0]);
+    return laptops.sort((a: Laptop, b: Laptop) => a.price - b.price);
   }
 }
---
+++ tests/services/sortLaptopsByAscendingPriceService.test.ts
@@ -0,0 +1,37 @@
+import Laptop from "entities/laptop";
+import SortLaptopsByAscendingPriceService from "services/sortLaptopsByAscendingPriceService";
+import Reviews from "valueObjects/reviews";
+
+const buildLaptopMock = (price: number) =>
+  new Laptop({
+    brand: "",
+    model: "",
+    screen: "",
+    processor: "",
+    ram: "",
+    memory: "",
+    os: "",
+    hdd: "",
+    currency: "",
+    price: price,
+    reviews: new Reviews({ amount: 0, stars: 0 }),
+  });
+
+describe("SortLaptopsByAscendingPriceService", () => {
+  describe("execute", () => {
+    it("should return expected array", () => {
+      const first = buildLaptopMock(5);
+      const second = buildLaptopMock(10);
+      const third = buildLaptopMock(15);
+      const unsortedLaptops = [second, third, first];
+      const sortLaptopsByAscendingPriceService =
+        new SortLaptopsByAscendingPriceService();
+
+      const sortedLaptops =
+        sortLaptopsByAscendingPriceService.execute(unsortedLaptops);
+
+      const expectedSortedLaptops = [first, second, third];
+      expect(sortedLaptops).toStrictEqual(expectedSortedLaptops);
+    });
+  });
+});
