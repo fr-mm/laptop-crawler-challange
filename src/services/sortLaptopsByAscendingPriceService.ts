93ccdb5e049bc8cf60ca8e20ad2f942469cef709
chico <chico.zimcolor@gmail.com>
2022-07-05 19:07:26 -0300

    added missing keyword

--- src/services/sortLaptopsByAscendingPriceService.ts
+++ src/services/sortLaptopsByAscendingPriceService.ts
@@ -1,7 +1,7 @@
 import Laptop from "entities/laptop";
 
 export default class SortLaptopsByAscendingPriceService {
-  execute(laptops: Laptop[]): Laptop[] {
+  public execute(laptops: Laptop[]): Laptop[] {
     return laptops.sort((a: Laptop, b: Laptop) => a.price - b.price);
   }
 }
